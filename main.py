5d0932ae011904747443cfafe67c849418cc5a7b
Mauricio Vargas <38122573+m4wnn@users.noreply.github.com>
2024-11-11 00:12:21 -0800

    feat: add main.py with preliminary ImmigrationEffectFlow DAG, including initial data loading and transformation step

---
+++ main.py
@@ -0,0 +1,49 @@
+from metaflow import FlowSpec, step
+
+
+class ImmigrationEffectFlow(FlowSpec):
+
+    @step
+    def start(self):
+        pass
+        self.next(self.create_dataset)
+
+    @step
+    def create_dataset(self):
+        from src.data_processing import census_cz_merge
+
+        self.data = census_cz_merge()
+        self.next(self.fit_instrument_1, self.fit_instrument_2)
+
+    @step
+    def fit_instrument_1(self):
+        pass
+        self.next(self.test_instrument_1)
+
+    @step
+    def fit_instrument_2(self):
+        pass
+        self.next(self.test_instrument_2)
+
+    @step
+    def test_instrument_1(self):
+        pass
+        self.next(self.select_instrument)
+
+    @step
+    def test_instrument_2(self):
+        pass
+        self.next(self.select_instrument)
+
+    @step
+    def select_instrument(self, inputs):
+        pass
+        self.next(self.end)
+
+    @step
+    def end(self):
+        pass
+
+
+if __name__ == "__main__":
+    ImmigrationEffectFlow()
