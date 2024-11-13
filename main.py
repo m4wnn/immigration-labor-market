2e8c2662d52a7843cea509ee49c706f62306e3b3
Mauricio Vargas <38122573+m4wnn@users.noreply.github.com>
2024-11-13 00:34:10 -0800

    feat(main): add experiment to evaluate relevance of instruments with controls, constants, dependent variable, and covariance type

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 # %%
 from src.data_processing import DataPipeline
+import linearmodels
 
-# %%
 import warnings
+import statsmodels.api as sm
+from itertools import product
+import pandas as pd
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
+
+
 # %%
 data_pipeline = DataPipeline("data")
-data = data_pipeline.load_data()
-# %% Useful attributes in `data_pipeline`
-# data_pipeline._shock
-# data_pipeline._outcomes
-# data_pipeline._controls
-# data_pipeline._instruments
-data.head()
+data = data_pipeline.load_data(add_const=True)
+# %%
+grid = {
+    "dependent": ["Dln_wage", "Dunemp_rate", "Dnilf_rate"],
+    "instrument": ["z_1", "z_2"],
+    "cov_type": ["robust", "kernel", "unadjusted"],
+}
+# %%
+results = {"y": [], "z": [], "cov_type": [], "iv": []}
+for y, z, cov_type in product(*grid.values()):
+    results["y"].append(y)
+    results["z"].append(z)
+    results["cov_type"].append(cov_type)
+    results["iv"].append(
+        linearmodels.IV2SLS(
+            dependent=data_pipeline._outcomes[y],
+            endog=data_pipeline._shock,
+            exog=sm.add_constant(data_pipeline._controls),
+            instruments=data_pipeline._instruments[z],
+        ).fit(cov_type=cov_type)
+    )
+results = pd.DataFrame(results)
+
+results["partial_f_stat"] = [
+    fs.first_stage.diagnostics["f.stat"]["x"] for fs in results.iv
+]
+results["weak_instrument"] = [True if w < 10 else False for w in results.partial_f_stat]
+# %%
+results
