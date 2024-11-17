563c49564f8ff08e81f26e96551673ad1b558373
Mauricio Vargas <38122573+m4wnn@users.noreply.github.com>
2024-11-16 19:04:51 -0800

    feat: add 2SLS estimation for all dependent variables and brief analysis of instrument relevance

--- main.py
+++ main.py
@@ -1,43 +1,71 @@
 # %%
-from src.data_processing import DataPipeline
+import pandas as pd
 import linearmodels
 
-import warnings
-import statsmodels.api as sm
+from rich import inspect
 from itertools import product
-import pandas as pd
+import pickle
+
+from src.data_processing import DataPipeline
+
+import warnings
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
-
-
 # %%
 data_pipeline = DataPipeline("data")
 data = data_pipeline.load_data(add_const=True)
 # %%
-grid = {
-    "dependent": ["Dln_wage", "Dunemp_rate", "Dnilf_rate"],
-    "instrument": ["z_1", "z_2"],
-    "cov_type": ["robust", "kernel", "unadjusted"],
+controls_subsets = {
+    "const": ["const"],
+    "key": ["const", "fborn_share_80"],
+    "all": data_pipeline._controls.columns,
 }
 # %%
-results = {"y": [], "z": [], "cov_type": [], "iv": []}
-for y, z, cov_type in product(*grid.values()):
+grid = {
+    "dependent": ["Dln_wage", "Dunemp_rate", "Dnilf_rate"],
+    "controls": controls_subsets.keys(),
+    "instrument": ["z_1", "z_2"],
+}
+
+results = {"y": [], "controls": [], "z": [], "iv": []}
+
+for y, controls, z in product(*grid.values()):
     results["y"].append(y)
+    results["controls"].append(controls)
     results["z"].append(z)
-    results["cov_type"].append(cov_type)
     results["iv"].append(
         linearmodels.IV2SLS(
             dependent=data_pipeline._outcomes[y],
             endog=data_pipeline._shock,
-            exog=sm.add_constant(data_pipeline._controls),
+            exog=data_pipeline._controls[controls_subsets[controls]],
             instruments=data_pipeline._instruments[z],
-        ).fit(cov_type=cov_type)
+        ).fit(cov_type="clustered", clusters=data.statefip)
     )
+
 results = pd.DataFrame(results)
 
 results["partial_f_stat"] = [
     fs.first_stage.diagnostics["f.stat"]["x"] for fs in results.iv
 ]
-results["weak_instrument"] = [True if w < 10 else False for w in results.partial_f_stat]
+
+results["partial_f_pval"] = [
+    fs.first_stage.diagnostics["f.pval"]["x"] for fs in results.iv
+]
+
+results["weak_instrument_pval"] = [
+    True if w > 0.005 else False for w in results.partial_f_pval
+]
+
+results["weak_instrument_approx"] = [
+    True if w < 10 else False for w in results.partial_f_stat
+]
+
+results["pval_and_aprox"] = [
+    a and b
+    for (a, b) in zip(results.weak_instrument_pval, results.weak_instrument_approx)
+]
+
 # %%
-results
+
+with open("data/iv_results.pkl", "wb") as file:
+    pickle.dump(results, file)
